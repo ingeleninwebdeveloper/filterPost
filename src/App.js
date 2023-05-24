b70e31ca36c4ffb8e9ab5e64fb0c0903f202e22d
Lenin <ingeleninfernando@gmail.com>
2023-05-23 22:13:45 -0500

    test_ok

--- src/App.js
+++ src/App.js
@@ -5,18 +5,49 @@ import posts from './posts'
 // Modifica el componente App para implmentar la funcionalidad requerida
 
 class App extends Component {
+
+  constructor() {
+
+    super() 
+
+      this.state = {
+        
+        dataFilter: ''
+
+      };
+
+    }
+
+    handleChange(e) {
+      this.setState({
+        dataFilter: e.target.value
+      
+      }) 
+
+    }
+
+  
+  
   render() {
     return (
       <div>
         <div className="filter">
-          <input type="text" placeholder="Ingresa el término de búsqueda" />
+          <input type="text" placeholder="Ingresa el término de búsqueda" value={this.state.dataFilter || ""} onChange={this.handleChange.bind(this)} />
         </div>
         <ul>
-          <li>
-            <a href={posts[0].url}><img src={posts[0].image } /></a>
-            <p>{ posts[0].title }</p>
-          </li>
+          {posts.filter((item)=> {
+            return this.state.dataFilter.toLowerCase()===''?
+            item
+            : item.title.toLowerCase().includes(this.state.dataFilter)
+          }).map(function (d, idx){
+            return (<li key = {idx}>
+              <a href={d.url}><img src= {d.image}  alt=""/></a>
+              <p>{ d.title }</p>
+            </li>)
+          })}
+          
         </ul>
+      <span>{this.state.dataFilter}</span>  
       </div>
     )
   }
